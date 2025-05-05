76b822aeaa5f4127dd9ce6872083648482ae3e8c
faizankhan786123 <faizankh786123@gmail.com>
2025-05-05 10:25:27 +0400

    Add files via upload

---
+++ sqlfile.sql
@@ -0,0 +1,6 @@
+strQuery1 = "SELECT top 10 " + select_column + " FROM " + TableName + " with (nolock) WHERE " + ColNames
+					+ " = '" + ReadCode + "'";
+					
+					
+					
+select top 10 ALERT_INDEX, Alert_name, Alert_code, Mobile_no, Alert_text, Alert_Status,wi_Name from NG_RLOS_SMSQUEUETABLE with (nolock) where ALERT_STATUS ='p';
