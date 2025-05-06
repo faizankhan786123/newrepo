406a331bb108a7f1e91e7f7b55a97ba70cff8a08
faizankhan786123 <faizankh786123@gmail.com>
2025-05-06 22:30:11 +0530

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -6,3 +6,13 @@ strQuery1 = "SELECT top 10 " + select_column + " FROM " + TableName + " with (no
 select top 10 ALERT_INDEX, Alert_name, Alert_code, Mobile_no, Alert_text, Alert_Status,wi_Name from NG_RLOS_SMSQUEUETABLE with (nolock) where ALERT_STATUS ='p';
 
 				mLogger.info("Complete ArrayList Values: " + ArrLstTableValues.toString());
+
+
+CREATE TABLE NG_INFOBIP_AUDIT_LOG (
+    WORKITEM_NO     VARCHAR(100),
+    INPUT_XML       TEXT,
+    OUTPUT_XML      TEXT,
+    RESPONSE_CODE   VARCHAR(50),
+    INSERTED_ON     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
