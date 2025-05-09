75be23d5febdfae1bfc959a572d9ae2f40c9f736
faizankhan786123 <faizankh786123@gmail.com>
2025-05-09 15:46:56 +0530

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -19,3 +19,36 @@ CREATE TABLE NG_INFOBIP_AUDIT_LOG (
 UPDATE NG_INFOBIP_AUDIT_LOG
 SET RESPONSE_CODE = '500'
 WHERE WORKITEM_NO = 'W12345';
+
+CREATE SEQUENCE NG_INFOBIP_EVENTID_SEQ
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
+
+
+
+
+CREATE TABLE USR_0_INFOBIP_SMS_QUEUETABLE (
+    Infobip_EventID            NUMBER PRIMARY KEY,
+    Processname                NVARCHAR(100),
+    WI_NAME                    NVARCHAR(100),
+    AlertID                    NVARCHAR(100),
+    InsertedDateTime           NVARCHAR(50),
+    CIF                        NVARCHAR(50),
+    Dynamic_Tags               NVARCHAR(400), -- $ or ~ separated
+    Dynamic_Values             NVARCHAR(400), -- ~#$~ separated
+    Trigger_Status             CHAR(1),        -- P, D, F
+    Infobip_Response_Code      NVARCHAR(50),
+    Infobip_Response_Message   NVARCHAR(1000),
+    Infobip_No_Of_Retry        NUMBER
+);
+
+
+CREATE TABLE NG_INFOBIP_JSON_LOGHISTORY (
+    infobip_eventID     NUMBER PRIMARY KEY,
+    Request_time        TIMESTAMP DEFAULT SYSTIMESTAMP,
+    Response_Time       TIMESTAMP,
+    Request_JSON        CLOB,
+    Response_JSON       CLOB
+);
