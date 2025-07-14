bd0e066d9dd7c563445bba5fde28998287e550ab
faizankhan786123 <faizankh786123@gmail.com>
2025-07-14 12:12:25 +0530

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,11 +1,9 @@
- if (!"".equalsIgnoreCase(SMS_MOBNO) &&
-				 !"".equalsIgnoreCase(sms_template)) {
-				 SMSquery = "Insert into NG_RLOS_SMSQUEUETABLE"
-				+ (Alert_Name,Alert_Code,ALert_Status,Mobile_No,Alert_Text,WI_Name,Workstep_Name,inserted_Date_time)
-				 values ('TS SMS','TS','P','"
-				 + SMS_MOBNO + "','" + sms_template + "', '" +
-				 getWorkitemName(iformObj) + "','"
-				 + iformObj.getActivityName() + "', getdate())";
-				 int saveDataInDB1 = iformObj.saveDataInDB(SMSquery);
-				 TS.mLogger.debug("Query:" + SMSquery + saveDataInDB1);
-				 }
+if (!"".equalsIgnoreCase(SMS_MOBNO) && !"".equalsIgnoreCase(sms_template)) {
+    String SMSquery = "INSERT INTO NG_RLOS_SMSQUEUETABLE " +
+        "(Alert_Name, Alert_Code, ALert_Status, Mobile_No, Alert_Text, WI_Name, Workstep_Name, inserted_Date_time) " +
+        "VALUES ('TS SMS', 'TS', 'P', '" + SMS_MOBNO + "', '" + sms_template + "', '" +
+        getWorkitemName(iformObj) + "', '" + iformObj.getActivityName() + "', GETDATE())";
+
+    int saveDataInDB1 = iformObj.saveDataInDB(SMSquery);
+    TS.mLogger.debug("Query: " + SMSquery + " | Result: " + saveDataInDB1);
+}
