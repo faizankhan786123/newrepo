97fd9e4930570194da1fa218374162195ed5a709
faizankhan786123 <faizankh786123@gmail.com>
2025-07-14 10:40:26 +0400

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,5 +1,11 @@
-select CIF from RB_DSR_ODC_EXTTABLE
-
-
-
-			String Query = "Select * From USR_0_CSR_BT_TemplateMapping where ProcessName = 'DSR_ODC' and TemplateType = '"
+ if (!"".equalsIgnoreCase(SMS_MOBNO) &&
+				 !"".equalsIgnoreCase(sms_template)) {
+				 SMSquery = "Insert into NG_RLOS_SMSQUEUETABLE"
+				+ (Alert_Name,Alert_Code,ALert_Status,Mobile_No,Alert_Text,WI_Name,Workstep_Name,inserted_Date_time)
+				 values ('TS SMS','TS','P','"
+				 + SMS_MOBNO + "','" + sms_template + "', '" +
+				 getWorkitemName(iformObj) + "','"
+				 + iformObj.getActivityName() + "', getdate())";
+				 int saveDataInDB1 = iformObj.saveDataInDB(SMSquery);
+				 TS.mLogger.debug("Query:" + SMSquery + saveDataInDB1);
+				 }
