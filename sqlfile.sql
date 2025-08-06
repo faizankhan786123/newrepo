6781b17ecd9cb3301678a68ffd1b24a8e32b3c13
faizankhan786123 <faizankh786123@gmail.com>
2025-08-06 20:50:55 +0400

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,96 +1,73 @@
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name~dDMMYYYY
+if (!txtMessage.equalsIgnoreCase("NULL") && infobipIsActive.equalsIgnoreCase("N")) {
+					CSR_CCC.mLogger.debug("txtMessage before replace" + txtMessage);
+					txtMessage = txtMessage.replaceAll("#WI_No#", split_WI_No);
+					txtMessage = txtMessage.replaceAll("#Amount#", Amount);
+					txtMessage = txtMessage.replaceAll("#Card_No#", lastDigitCard_No);
+					txtMessage = txtMessage.replaceAll("#DD/MM/YYYY#", date);
+					CSR_CCC.mLogger.debug("txtMessage after replace" + txtMessage);
 
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name~dDMMYYYY
+					String tableName = "NG_RLOS_SMSQUEUETABLE";
+					String ALERT_Name = stage;
+					String Alert_Code = "CSR_CCC";
+					String Alert_Status = "P";
+					String Mobile_No = param[1];
+					CSR_CCC.mLogger.debug("Mobile_No--------->" + Mobile_No);
+					String Workstep_Name = (String) iform.getActivityName();
+					String columnName = "(ALERT_Name, Alert_Code, Alert_Status, Mobile_No, Alert_Text, WI_Name, Workstep_Name, Inserted_Date_time)";
+					String values = "('" + ALERT_Name + "','" + Alert_Code + "','" + Alert_Status + "','" + Mobile_No + "','" + txtMessage
+							+ "','" + WI_No + "','" + Workstep_Name + "', getdate() )";
+					String SMSInsertQuery = "Insert into " + tableName + " " + columnName + " values " + values;
+					CSR_CCC.mLogger.debug("Query to be inserted in table-----------------: " + SMSInsertQuery);
+					int status = iform.saveDataInDB(SMSInsertQuery);
+					CSR_CCC.mLogger.debug("SMS Triggered successfully if value of status is 1--------STATUS = " + status);
+					if (status == 1)
+						return "true";
+				} else if (infobipIsActive.equalsIgnoreCase("Y")) {
+					String DynamicValues = "";
+					String[] tags = DynamicTags.split("~");
+					CSR_CCC.mLogger.debug("Dynamic Tag Arr: " + Arrays.toString(tags));
 
-card_No~wI_No~cUR_Amount~merchant_Name
-card_No~wI_No~sub_Process_Name~dDMMYYYY
+					List<String> valueList = new ArrayList<String>();
+					for (String tag1 : tags) {
+						String pValue = "";
+						switch (tag1.trim()) {
+							case "card_No":
+								pValue = lastDigitCard_No;
+								break;
+							case "wI_No":
+								pValue = split_WI_No;
+								break;
+							case "dDMMYYYY":
+								pValue = date;
+								break;
+							case "amount":
+								pValue = Amount;
+								break;
+						}
+						valueList.add(pValue);
+					}
+					DynamicValues = String.join("~#~", valueList);
+					CSR_CCC.mLogger.debug("Final List of Dynamic Values: " + valueList);
 
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name~dDMMYYYY
-
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name~dDMMYYYY~oS_Amount
-card_No~wI_No~sub_Process_Name~dDMMYYYY
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~cUR_Amount~merchant_Name
-card_No~wI_No~dDMMYYYY~cUR_Amount~merchant_Name
-card_No~wI_No~cUR_Amount~merchant_Name
-card_No~wI_No~cUR_Amount~merchant_Name
-card_No~wI_No~cUR_Amount~merchant_Name
-card_No~wI_No~cUR_Amount~merchant_Name
-card_No~wI_No~cUR_Amount~school_Name
-card_No~wI_No~cUR_Amount~school_Name~dDMMYYYY
-card_No~wI_No
-card_No~wI_No~cUR_Amount~school_Name
-card_No~wI_No~cUR_Amount~school_Name
-card_No~wI_No~sub_Process_Name~cUR_Amount~sLA_TAT
-
-card_No~wI_No~sub_Process_Name~cUR_Amount
-
-
-
-card_No~wI_No~sub_Process_Name~cUR_Amount
-card_No~wI_No~sub_Process_Name~cUR_Amount~sLA_TAT
-card_No~wI_No~sub_Process_Name~cUR_Amount
-
-
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name
-
-
-card_No~wI_No~sub_Process_Name~sLA_TAT
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name~dDMMYYYY
-
-
-
-
-
-
-
-
-
-
-
-
-card_No~wI_No~sub_Process_Name
-
-
-card_No~wI_No~sub_Process_Name
-
-
-card_No~wI_No~sub_Process_Name~cUR_Amount
-card_No~wI_No~sub_Process_Name~sLA_TAT
-card_No~wI_No~sub_Process_Name
-
-
-
-
-card_No~wI_No~sub_Process_Name~dDMMYYYY
-
-card_No~wI_No~sub_Process_Name~sLA_TAT~cUR_Amount
-
-
-card_No~wI_No~sub_Process_Name~cUR_Amount
-card_No~wI_No~sub_Process_Name~cUR_Amount
-card_No~wI_No~sub_Process_Name~cUR_Amount
-card_No~wI_No~sub_Process_Name~sLA_TAT
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name~dDMMYYYY
-
-card_No~wI_No~sub_Process_Name~sLA_TAT
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name~dDMMYYYY
-
-
-
-card_No~wI_No~sub_Process_Name~sLA_TAT
-card_No~wI_No~sub_Process_Name
-card_No~wI_No~sub_Process_Name~dDMMYYYY
-
-
+					String tableName = "USR_0_INFOBIP_SMS_QUEUETABLE";
+					String ALERT_Name = stage;
+					String ProcessName = "CSR_CCC";
+					String Alert_Status = "P";
+					String Mobile_No = param[1];
+					CSR_CCC.mLogger.debug("Mobile no--------->" + Mobile_No);
+					String Workstep_Name = (String) iform.getActivityName();
+					String columnName = "(Processname,WI_NAME,AlertID,InsertedDateTime,CIF,Dynamic_Tags,Dynamic_Values,Alert_Status)";
+					String values = "('" + ProcessName + "','" + WI_No + "','" + AlertID + "',format(getdate(),'yyyy-MM-dd HH:mm:ss.fff'),'" + CIF
+							+ "','" + DynamicTags + "','" + DynamicValues + "','" + Alert_Status + "')";
+					String SMSInsertQuery = "Insert into " + tableName + " " + columnName + " values " + values;
+					CSR_CCC.mLogger.debug("Query to be inserted in table-----------------: " + SMSInsertQuery);
+					int status = iform.saveDataInDB(SMSInsertQuery);
+					CSR_CCC.mLogger.debug("SMS Triggered successfully if value of status is 1--------STATUS = " + status);
+					if (status == 1)
+						return "true";
+				}
+			}
+		} catch (Exception ex) {
+			CSR_CCC.mLogger.debug("Some error in sendSMScall" + ex.toString());
+			return "false";
+		}
