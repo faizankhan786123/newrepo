47134fd3d77ca431659143f5fcb5cfc39e4cb5e2
faizankhan786123 <faizankh786123@gmail.com>
2025-08-06 19:34:10 +0400

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,66 +1,96 @@
-TS.mLogger.debug("Sub Process Name : " + Sub_Process_Name);
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name~dDMMYYYY
 
-// Prepare tag to column map
-String updateDate = tentative_date;
-String wiName = getWorkitemName(iformObj);
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name~dDMMYYYY
 
-Map<String, String> tagToColumnMap = new HashMap<>();
-tagToColumnMap.put("cARD_CODE", card_code);
-tagToColumnMap.put("wI_NAME", wiName);
-tagToColumnMap.put("dISPUTE_RELATED", "CARD_DISPUTE_TYPE"); // from NG_TS_EXTTABLE
-tagToColumnMap.put("sUB_PROCESS_NAME", Sub_Process_Name);
-tagToColumnMap.put("uPDATE_DATE", updateDate);
-tagToColumnMap.put("aMOUNT_AED", "AMT_IN_AED"); // from NG_TS_TRANS_DISPUTE_GRID_DTLS
+card_No~wI_No~cUR_Amount~merchant_Name
+card_No~wI_No~sub_Process_Name~dDMMYYYY
 
-TS.mLogger.debug("tag To ColumnMap : " + tagToColumnMap);
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name~dDMMYYYY
 
-// Split dynamic tags
-String[] dynamic_tags = infobip_dynamic_tags.split("~");
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name~dDMMYYYY~oS_Amount
+card_No~wI_No~sub_Process_Name~dDMMYYYY
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~cUR_Amount~merchant_Name
+card_No~wI_No~dDMMYYYY~cUR_Amount~merchant_Name
+card_No~wI_No~cUR_Amount~merchant_Name
+card_No~wI_No~cUR_Amount~merchant_Name
+card_No~wI_No~cUR_Amount~merchant_Name
+card_No~wI_No~cUR_Amount~merchant_Name
+card_No~wI_No~cUR_Amount~school_Name
+card_No~wI_No~cUR_Amount~school_Name~dDMMYYYY
+card_No~wI_No
+card_No~wI_No~cUR_Amount~school_Name
+card_No~wI_No~cUR_Amount~school_Name
+card_No~wI_No~sub_Process_Name~cUR_Amount~sLA_TAT
 
-// Prepare tag value map
-Map<String, String> tagValueMap = new HashMap<>();
-tagValueMap.put("sLA_TAT", SLA); // DIRECTLY injecting SLA value
-TS.mLogger.debug("TAG TO Column : " + tagToColumnMap);
+card_No~wI_No~sub_Process_Name~cUR_Amount
 
-// Separate columns for each table
-Set<String> extColsFromEXTTABLE = new HashSet<>();
-Set<String> extColsFromDISPUTE = new HashSet<>();
 
-for (String tag : dynamic_tags) {
-    String column = tagToColumnMap.get(tag);
-    if (column == null) continue;
 
-    if ("CARD_DISPUTE_TYPE".equals(column)) {
-        extColsFromEXTTABLE.add(column);
-    } else if ("AMT_IN_AED".equals(column)) {
-        extColsFromDISPUTE.add(column);
-    }
-}
+card_No~wI_No~sub_Process_Name~cUR_Amount
+card_No~wI_No~sub_Process_Name~cUR_Amount~sLA_TAT
+card_No~wI_No~sub_Process_Name~cUR_Amount
 
-// Fetch from NG_TS_EXTTABLE
-if (!extColsFromEXTTABLE.isEmpty()) {
-    String colStr = String.join(",", extColsFromEXTTABLE);
-    String sql = "SELECT " + colStr + " FROM NG_TS_EXTTABLE WITH(NOLOCK) WHERE WI_NAME = '" + wiName + "'";
-    List extResult = iformObj.getDataFromDB(sql);
-    if (!extResult.isEmpty()) {
-        List<String> row = (List<String>) extResult.get(0);
-        int i = 0;
-        for (String col : extColsFromEXTTABLE) {
-            tagValueMap.put(getTagByValue(tagToColumnMap, col), row.get(i++));
-        }
-    }
-}
 
-// Fetch from NG_TS_TRANS_DISPUTE_GRID_DTLS
-if (!extColsFromDISPUTE.isEmpty()) {
-    String colStr = String.join(",", extColsFromDISPUTE);
-    String sql = "SELECT " + colStr + " FROM NG_TS_TRANS_DISPUTE_GRID_DTLS WITH(NOLOCK) WHERE WI_NAME = '" + wiName + "'";
-    List extResult = iformObj.getDataFromDB(sql);
-    if (!extResult.isEmpty()) {
-        List<String> row = (List<String>) extResult.get(0);
-        int i = 0;
-        for (String col : extColsFromDISPUTE) {
-            tagValueMap.put(getTagByValue(tagToColumnMap, col), row.get(i++));
-        }
-    }
-}
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name
+
+
+card_No~wI_No~sub_Process_Name~sLA_TAT
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name~dDMMYYYY
+
+
+
+
+
+
+
+
+
+
+
+
+card_No~wI_No~sub_Process_Name
+
+
+card_No~wI_No~sub_Process_Name
+
+
+card_No~wI_No~sub_Process_Name~cUR_Amount
+card_No~wI_No~sub_Process_Name~sLA_TAT
+card_No~wI_No~sub_Process_Name
+
+
+
+
+card_No~wI_No~sub_Process_Name~dDMMYYYY
+
+card_No~wI_No~sub_Process_Name~sLA_TAT~cUR_Amount
+
+
+card_No~wI_No~sub_Process_Name~cUR_Amount
+card_No~wI_No~sub_Process_Name~cUR_Amount
+card_No~wI_No~sub_Process_Name~cUR_Amount
+card_No~wI_No~sub_Process_Name~sLA_TAT
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name~dDMMYYYY
+
+card_No~wI_No~sub_Process_Name~sLA_TAT
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name~dDMMYYYY
+
+
+
+card_No~wI_No~sub_Process_Name~sLA_TAT
+card_No~wI_No~sub_Process_Name
+card_No~wI_No~sub_Process_Name~dDMMYYYY
+
+
