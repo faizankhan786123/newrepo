8a177494122f2c526bfeb291507aabe2090258eb
faizankhan786123 <faizankh786123@gmail.com>
2025-08-08 20:00:53 +0530

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,6 +1,2 @@
- -- Get last 4 digits of the card number
-    SET @DynamicValues = RIGHT(@HEADER_CARD_NO, 4)
-
-    PRINT 'CIF ID IS = ' + @CIF_ID 
-    PRINT 'Card Number  = ' + @HEADER_CARD_NO
-    PRINT 'Last 4 Digits (DynamicValues) = ' + @DynamicValues
+ALTER TABLE table_name
+ADD column_name datatype;
