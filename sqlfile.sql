e0f527c0533142a29cf95f21893ad2fde750c2bd
faizankhan786123 <faizankh786123@gmail.com>
2025-08-09 09:52:08 +0530

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,13 +1,47 @@
-UPDATE USR_0_CSR_BT_TemplateMapping SET Infobip_Alert_ID = 'BPMINTRO2' , Infobip_Dynamic_Tags = 'card_No~wI_No~sLA_TAT',Infobip_SMS_isActive='Y' where ProcessName='DSR_MR' and MailSubject='We have received your request now!';
+-- 1
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMINTRO2',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sLA_TAT',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_MR'
+  AND MailSubject = 'We have received your request now!';
 
+-- 2
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BMPCOMP2',
+    Infobip_Dynamic_Tags = 'card_No~wI_No',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_MR'
+  AND MailSubject = 'We are Done! Your request is now approved.';
 
+-- 3
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPNREJECTRTS2',
+    Infobip_Dynamic_Tags = 'card_No~wI_No',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_MR'
+  AND MailSubject = '';
 
+-- 4
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMPENDING2',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~dDMMYYYY',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_MR'
+  AND MailSubject = 'Almost there! Let us finish your request.';
 
+-- 5
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMRJFNDISCARD',
+    Infobip_Dynamic_Tags = 'card_No~wI_No',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_MR'
+  AND MailSubject = 'Unfortunately, we cannot offer you the requested RAKBANK service at the moment.';
 
-We have received your request now!		     BPMINTRO2	card_No~wI_No~sLA_TAT	Y
-We are Done! Your request is now approved.		BMPCOMP2	card_No~wI_No	Y
-		BPNREJECTRTS2	card_No~wI_No	Y
-Almost there! Let us finish your request.		BPMPENDING2	card_No~wI_No~dDMMYYYY	Y
-Unfortunately, we cannot offer you the requested RAKBANK service at the moment.		BPMRJFNDISCARD	card_No~wI_No	Y
-Your RAKBANK request has been cancelled.		BPMPENCN	card_No~wI_No~cancellationReason	Y
-Y
+-- 6
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMPENCN',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~cancellationReason',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_MR'
+  AND MailSubject = 'Your RAKBANK request has been cancelled.';
