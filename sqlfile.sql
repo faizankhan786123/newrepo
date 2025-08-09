fdbc8efa764943277d0e7316ba0674a64c17d049
faizankhan786123 <faizankh786123@gmail.com>
2025-08-09 08:18:54 +0400

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,2 +1,13 @@
-ALTER TABLE table_name
-ADD column_name datatype;
+UPDATE USR_0_CSR_BT_TemplateMapping SET Infobip_Alert_ID = 'BPMINTRO2' , Infobip_Dynamic_Tags = 'card_No~wI_No~sLA_TAT',Infobip_SMS_isActive='Y' where ProcessName='DSR_MR' and MailSubject='We have received your request now!';
+
+
+
+
+
+We have received your request now!		     BPMINTRO2	card_No~wI_No~sLA_TAT	Y
+We are Done! Your request is now approved.		BMPCOMP2	card_No~wI_No	Y
+		BPNREJECTRTS2	card_No~wI_No	Y
+Almost there! Let us finish your request.		BPMPENDING2	card_No~wI_No~dDMMYYYY	Y
+Unfortunately, we cannot offer you the requested RAKBANK service at the moment.		BPMRJFNDISCARD	card_No~wI_No	Y
+Your RAKBANK request has been cancelled.		BPMPENCN	card_No~wI_No~cancellationReason	Y
+Y
