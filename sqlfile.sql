38c4441e6e0255f6639d90a1e724d5c256426299
faizankhan786123 <faizankh786123@gmail.com>
2025-08-09 10:00:21 +0530

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,23 +1,183 @@
-	TRANSACTION DISPUTE	NULL	NULL	N
-Unfortunately, we cannot offer you the requested RAKBANK service at the moment.	TRANSACTION DISPUTE	NULL	NULL	N
-We have received your request now!	RE-ISSUE OF PIN	BPMINTRO31	card_No~wI_No~sub_Process_Name~sLA_TAT	Y
-We are Done! Your request is now approved.	RE-ISSUE OF PIN	BPMCOMP31	card_No~wI_No~sub_Process_Name	Y
-	RE-ISSUE OF PIN	BPMREJRTS31	card_No~wI_No~sub_Process_Name	Y
-Unfortunately, we cannot offer you the requested RAKBANK service at the moment.	RE-ISSUE OF PIN	BPMREJFINDIS31	card_No~wI_No~sub_Process_Name	Y
-We have received your request now!	TRANSACTION DISPUTE	BPMINTRO32	card_No~wI_No	Y
-We are Done! Your request is now approved.	TRANSACTION DISPUTE	BPMCOMP32	card_No~wI_No~amount	Y
-Almost there! Let us finish your request.	TRANSACTION DISPUTE	BPMPEN32	card_No~wI_No~dDMMYYYY	Y
-We have received your request now!	CARD DELIVERY REQUEST	BPMINTRO33	card_No~wI_No~sub_Process_Name~sLA_TAT	Y
-We are Done! Your request is now approved.	CARD DELIVERY REQUEST	BPMCOMP33	card_No~wI_No~sub_Process_Name~sLA_TAT	Y
-	CARD DELIVERY REQUEST	BPMREJRTS31	card_No~wI_No~sub_Process_Name	Y
-Unfortunately, we cannot offer you the requested RAKBANK service at the moment.	CARD DELIVERY REQUEST	BPMREJFINDIS31	card_No~wI_No~sub_Process_Name	Y
-Almost there! Let us finish your request.	CARD DELIVERY REQUEST	BPMPEN34	card_No~wI_No~sub_Process_Name~dDMMYYYY	Y
-We have received your request now!	CARD REPLACEMENT	BPMINTRO34	card_No~wI_No~sub_Process_Name	Y
-We are Done! Your request is now approved.	CARD REPLACEMENT	BPMCOMP34	card_No~wI_No~sub_Process_Name	Y
-	CARD REPLACEMENT	BPMREJRTS34	card_No~wI_No~sub_Process_Name	Y
-Unfortunately, we cannot offer you the requested RAKBANK service at the moment.	CARD REPLACEMENT	BPMREJFINDIS31	card_No~wI_No~sub_Process_Name	Y
-Almost there! Let us finish your request.	CARD REPLACEMENT	BPMPEN33	card_No~wI_No~sub_Process_Name~dDMMYYYY	Y
-We have received your request now!	EARLY CARD RENEWAL	BPMINTRO34	card_No~wI_No~sub_Process_Name	Y
-We are Done! Your request is now approved.	EARLY CARD RENEWAL	BPMCOMP34	card_No~wI_No~sub_Process_Name	Y
-	EARLY CARD RENEWAL	BPMPEN33	card_No~wI_No~sub_Process_Name~dDMMYYYY	Y
-Unfortunately, we cannot offer you the requested RAKBANK service at the moment.	EARLY CARD RENEWAL	BPMREJFINDIS31	card_No~wI_No~sub_Process_Name	Y
+-- 1
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = NULL,
+    Infobip_Dynamic_Tags = NULL,
+    Infobip_SMS_isActive = 'N'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'TRANSACTION DISPUTE';
+
+-- 2
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = NULL,
+    Infobip_Dynamic_Tags = NULL,
+    Infobip_SMS_isActive = 'N'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'Unfortunately, we cannot offer you the requested RAKBANK service at the moment.';
+
+-- 3
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMINTRO31',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name~sLA_TAT',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'We have received your request now!';
+
+-- 4
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMCOMP31',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'We are Done! Your request is now approved.';
+
+-- 5
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMREJRTS31',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = '';
+
+-- 6
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMREJFINDIS31',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'Unfortunately, we cannot offer you the requested RAKBANK service at the moment.';
+
+-- 7
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMINTRO32',
+    Infobip_Dynamic_Tags = 'card_No~wI_No',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'We have received your request now!';
+
+-- 8
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMCOMP32',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~amount',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'We are Done! Your request is now approved.';
+
+-- 9
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMPEN32',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~dDMMYYYY',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'Almost there! Let us finish your request.';
+
+-- 10
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMINTRO33',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name~sLA_TAT',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'We have received your request now!';
+
+-- 11
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMCOMP33',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name~sLA_TAT',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'We are Done! Your request is now approved.';
+
+-- 12
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMREJRTS31',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = '';
+
+-- 13
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMREJFINDIS31',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'Unfortunately, we cannot offer you the requested RAKBANK service at the moment.';
+
+-- 14
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMPEN34',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name~dDMMYYYY',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'Almost there! Let us finish your request.';
+
+-- 15
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMINTRO34',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'We have received your request now!';
+
+-- 16
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMCOMP34',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'We are Done! Your request is now approved.';
+
+-- 17
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMREJRTS34',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = '';
+
+-- 18
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMREJFINDIS31',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'Unfortunately, we cannot offer you the requested RAKBANK service at the moment.';
+
+-- 19
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMPEN33',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name~dDMMYYYY',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'Almost there! Let us finish your request.';
+
+-- 20
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMINTRO34',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'We have received your request now!';
+
+-- 21
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMCOMP34',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'We are Done! Your request is now approved.';
+
+-- 22
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMPEN33',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name~dDMMYYYY',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = '';
+
+-- 23
+UPDATE USR_0_CSR_BT_TemplateMapping
+SET Infobip_Alert_ID = 'BPMREJFINDIS31',
+    Infobip_Dynamic_Tags = 'card_No~wI_No~sub_Process_Name',
+    Infobip_SMS_isActive = 'Y'
+WHERE ProcessName = 'DSR_ODC'
+  AND MailSubject = 'Unfortunately, we cannot offer you the requested RAKBANK service at the moment.';
