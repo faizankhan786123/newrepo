35c4840eb3221cb0be1c7acd3400bd7440a276b5
faizankhan786123 <faizankh786123@gmail.com>
2025-08-09 08:24:42 +0400

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,47 +1,23 @@
--- 1
-UPDATE USR_0_CSR_BT_TemplateMapping
-SET Infobip_Alert_ID = 'BPMINTRO2',
-    Infobip_Dynamic_Tags = 'card_No~wI_No~sLA_TAT',
-    Infobip_SMS_isActive = 'Y'
-WHERE ProcessName = 'DSR_MR'
-  AND MailSubject = 'We have received your request now!';
-
--- 2
-UPDATE USR_0_CSR_BT_TemplateMapping
-SET Infobip_Alert_ID = 'BMPCOMP2',
-    Infobip_Dynamic_Tags = 'card_No~wI_No',
-    Infobip_SMS_isActive = 'Y'
-WHERE ProcessName = 'DSR_MR'
-  AND MailSubject = 'We are Done! Your request is now approved.';
-
--- 3
-UPDATE USR_0_CSR_BT_TemplateMapping
-SET Infobip_Alert_ID = 'BPNREJECTRTS2',
-    Infobip_Dynamic_Tags = 'card_No~wI_No',
-    Infobip_SMS_isActive = 'Y'
-WHERE ProcessName = 'DSR_MR'
-  AND MailSubject = '';
-
--- 4
-UPDATE USR_0_CSR_BT_TemplateMapping
-SET Infobip_Alert_ID = 'BPMPENDING2',
-    Infobip_Dynamic_Tags = 'card_No~wI_No~dDMMYYYY',
-    Infobip_SMS_isActive = 'Y'
-WHERE ProcessName = 'DSR_MR'
-  AND MailSubject = 'Almost there! Let us finish your request.';
-
--- 5
-UPDATE USR_0_CSR_BT_TemplateMapping
-SET Infobip_Alert_ID = 'BPMRJFNDISCARD',
-    Infobip_Dynamic_Tags = 'card_No~wI_No',
-    Infobip_SMS_isActive = 'Y'
-WHERE ProcessName = 'DSR_MR'
-  AND MailSubject = 'Unfortunately, we cannot offer you the requested RAKBANK service at the moment.';
-
--- 6
-UPDATE USR_0_CSR_BT_TemplateMapping
-SET Infobip_Alert_ID = 'BPMPENCN',
-    Infobip_Dynamic_Tags = 'card_No~wI_No~cancellationReason',
-    Infobip_SMS_isActive = 'Y'
-WHERE ProcessName = 'DSR_MR'
-  AND MailSubject = 'Your RAKBANK request has been cancelled.';
+	TRANSACTION DISPUTE	NULL	NULL	N
+Unfortunately, we cannot offer you the requested RAKBANK service at the moment.	TRANSACTION DISPUTE	NULL	NULL	N
+We have received your request now!	RE-ISSUE OF PIN	BPMINTRO31	card_No~wI_No~sub_Process_Name~sLA_TAT	Y
+We are Done! Your request is now approved.	RE-ISSUE OF PIN	BPMCOMP31	card_No~wI_No~sub_Process_Name	Y
+	RE-ISSUE OF PIN	BPMREJRTS31	card_No~wI_No~sub_Process_Name	Y
+Unfortunately, we cannot offer you the requested RAKBANK service at the moment.	RE-ISSUE OF PIN	BPMREJFINDIS31	card_No~wI_No~sub_Process_Name	Y
+We have received your request now!	TRANSACTION DISPUTE	BPMINTRO32	card_No~wI_No	Y
+We are Done! Your request is now approved.	TRANSACTION DISPUTE	BPMCOMP32	card_No~wI_No~amount	Y
+Almost there! Let us finish your request.	TRANSACTION DISPUTE	BPMPEN32	card_No~wI_No~dDMMYYYY	Y
+We have received your request now!	CARD DELIVERY REQUEST	BPMINTRO33	card_No~wI_No~sub_Process_Name~sLA_TAT	Y
+We are Done! Your request is now approved.	CARD DELIVERY REQUEST	BPMCOMP33	card_No~wI_No~sub_Process_Name~sLA_TAT	Y
+	CARD DELIVERY REQUEST	BPMREJRTS31	card_No~wI_No~sub_Process_Name	Y
+Unfortunately, we cannot offer you the requested RAKBANK service at the moment.	CARD DELIVERY REQUEST	BPMREJFINDIS31	card_No~wI_No~sub_Process_Name	Y
+Almost there! Let us finish your request.	CARD DELIVERY REQUEST	BPMPEN34	card_No~wI_No~sub_Process_Name~dDMMYYYY	Y
+We have received your request now!	CARD REPLACEMENT	BPMINTRO34	card_No~wI_No~sub_Process_Name	Y
+We are Done! Your request is now approved.	CARD REPLACEMENT	BPMCOMP34	card_No~wI_No~sub_Process_Name	Y
+	CARD REPLACEMENT	BPMREJRTS34	card_No~wI_No~sub_Process_Name	Y
+Unfortunately, we cannot offer you the requested RAKBANK service at the moment.	CARD REPLACEMENT	BPMREJFINDIS31	card_No~wI_No~sub_Process_Name	Y
+Almost there! Let us finish your request.	CARD REPLACEMENT	BPMPEN33	card_No~wI_No~sub_Process_Name~dDMMYYYY	Y
+We have received your request now!	EARLY CARD RENEWAL	BPMINTRO34	card_No~wI_No~sub_Process_Name	Y
+We are Done! Your request is now approved.	EARLY CARD RENEWAL	BPMCOMP34	card_No~wI_No~sub_Process_Name	Y
+	EARLY CARD RENEWAL	BPMPEN33	card_No~wI_No~sub_Process_Name~dDMMYYYY	Y
+Unfortunately, we cannot offer you the requested RAKBANK service at the moment.	EARLY CARD RENEWAL	BPMREJFINDIS31	card_No~wI_No~sub_Process_Name	Y
