6ca2f2865e2813ba5e5835ea85fc152307312cc3
faizankhan786123 <faizankh786123@gmail.com>
2025-08-21 16:28:45 +0400

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -1,1 +1,10 @@
-EXEC sp_rename 'YourTableName.sms', 'SMS_Content', 'COLUMN';
+{
+  "properties": {
+    "dynamicKey1": "value1",
+    "dynamicKey2": "value2"
+  },
+  "fallbackDetails": {
+    "mobileNo": "971586660000",
+    "smsMessage": "Fallback SMS message in case of failure."
+  }
+}
